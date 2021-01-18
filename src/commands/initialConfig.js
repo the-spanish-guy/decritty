761372cbe8c2fe03ec1cb408e6403e82409da010
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2021-01-18 17:04:16 -0300

    removing unnecessary functions

--- src/commands/initialConfig.js
+++ src/commands/initialConfig.js
@@ -26,6 +26,10 @@ const copyFolderRecursive = (files, localFolder, destinationFolder) => {
 const copyTemplateFolders = () => {
   const templateFoler = path.join(__dirname, '..', 'templates')
   fs.mkdirSync(DEFAULT_THEME_FOLDER, { recursive: true })
+  fs.copyFileSync(
+    `${templateFoler}/${DEFAULT_FILE}`,
+    `${DEFAULT_FOLDER}/${DEFAULT_FILE}`
+  )
 
   const themes = fs.readdirSync(`${templateFoler}/themes/`)
   copyFolderRecursive(
@@ -40,25 +44,6 @@ const copyTemplateFolders = () => {
   )
 }
 
-const createDefaultTemplate = () => {
-  const templateFoler = path.join(__dirname, '..', 'templates')
-  fs.copyFileSync(
-    `${templateFoler}/${DEFAULT_FILE}`,
-    `${DEFAULT_FOLDER}/${DEFAULT_FILE}`
-  )
-  copyTemplateFolders()
-}
-
-const usingConfigOfUser = () => {
-  fs.mkdirSync(DEFAULT_FOLDER, { recursive: true })
-
-  fs.copyFileSync(
-    `${BKP_FOLDER}/${DEFAULT_FILE}`,
-    `${DEFAULT_FOLDER}/${DEFAULT_FILE}`
-  )
-  return copyTemplateFolders()
-}
-
 const createFolderIfNotExists = () => {
   if (fs.existsSync(BKP_FOLDER))
     return console.log(
@@ -67,12 +52,10 @@ const createFolderIfNotExists = () => {
   if (fs.existsSync(DEFAULT_FOLDER)) {
     fs.renameSync(DEFAULT_FOLDER, BKP_FOLDER)
     console.log(`the backup folder was created at: ${BKP_FOLDER} ${'\n'}`)
-    usingConfigOfUser()
-    return console.log('Done!' + '\n')
   }
 
   fs.mkdirSync(DEFAULT_FOLDER, { recursive: true })
-  createDefaultTemplate()
+  copyTemplateFolders()
   console.log('Done!' + '\n')
 }
 
