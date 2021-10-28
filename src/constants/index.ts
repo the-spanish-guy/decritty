532fa36ae6d159ea332604498abc2f02a931d68f
spanish <luyo.lemp@gmail.com>
2021-10-28 19:23:04 -0300

    refactor: added new ts file

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,6 @@
-const homedir = require('os').homedir()
+import { homedir as home } from 'os'
+
+const homedir = home()
 
 const DEFAULT_FOLDER = `${homedir}/.config/alacritty`
 const DEFAULT_SETTINGS_FOLDER = `${DEFAULT_FOLDER}/settings`
@@ -7,7 +9,7 @@ const BKP_FOLDER = `${homedir}/.config/old_alacritty`
 const DEFAULT_FILE = 'alacritty.yml'
 const FONT_FILE = 'fonts.yml'
 
-module.exports = {
+export {
   DEFAULT_FOLDER,
   DEFAULT_SETTINGS_FOLDER,
   DEFAULT_THEME_FOLDER,
