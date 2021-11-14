333258bb494f107b201485d0f111d5b3a53fa12e
spanish <luyo.lemp@gmail.com>
2021-11-14 17:41:14 -0300

    feat: added default configs

---
+++ src/commands/defaultConfigs.ts
@@ -0,0 +1,54 @@
+import { existsSync, renameSync, mkdirSync, copyFileSync, readdirSync } from 'fs'
+import { hex, rgb } from 'chalk'
+import { join } from 'path'
+
+import {
+  BKP_FOLDER,
+  DEFAULT_FILE,
+  DEFAULT_FOLDER,
+  DEFAULT_SETTINGS_FOLDER,
+  DEFAULT_THEME_FOLDER
+} from '../constants/index'
+import { cyan, red } from '../utils/colors'
+import { printMessage } from '../utils/message'
+
+export default class DefaultConfigs {
+  private static copyFolderRecursive = (
+    files: Array<string>,
+    localFolder: string,
+    destinationFolder: string
+  ) => {
+    files.map(file => copyFileSync(`${localFolder}/${file}`, `${destinationFolder}/${file}`))
+  }
+
+  static initialConfigs = () => {
+    try {
+      // criando pastas caso nao existam
+      if (existsSync(BKP_FOLDER)) {
+        printMessage('red', `Rename the existing backup folder and try again ${'\n'}`)
+        return
+      }
+
+      if (existsSync(DEFAULT_FOLDER)) {
+        renameSync(DEFAULT_FOLDER, BKP_FOLDER)
+        console.log('the backup folder was created at:')
+        printMessage('lightCyan', `${BKP_FOLDER}  ${'\n'}`)
+      }
+
+      mkdirSync(DEFAULT_FOLDER, { recursive: true })
+
+      // copiar o template
+
+      const templateFoler = join(__dirname, '..', 'templates')
+      mkdirSync(DEFAULT_THEME_FOLDER, { recursive: true })
+      copyFileSync(`${templateFoler}/${DEFAULT_FILE}`, `${DEFAULT_FOLDER}/${DEFAULT_FILE}`)
+
+      const themes = readdirSync(`${templateFoler}/themes/`)
+      this.copyFolderRecursive(themes, `${templateFoler}/themes`, `${DEFAULT_THEME_FOLDER}/`)
+
+      copyFileSync(`${templateFoler}/fonts.yml`, `${DEFAULT_SETTINGS_FOLDER}/fonts.yml`)
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
+}
