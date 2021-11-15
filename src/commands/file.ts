7964f40b4a78d3d2d77ff5aeed8a690e28f9c771
spanish <luyo.lemp@gmail.com>
2021-11-15 12:30:23 -0300

    feat: added some changes
    - added function to add a new font
    - refactor writeFile function

--- src/commands/file.ts
+++ src/commands/file.ts
@@ -1,23 +1,33 @@
 import { dump, load } from 'js-yaml'
 import { readFileSync, writeFile } from 'fs'
 import { FileType, FontType, PaddingType } from '../types/file'
-import { DEFAULT_FOLDER, DEFAULT_FILE } from '../constants/index'
+import { DEFAULT_FOLDER, DEFAULT_FILE, DEFAULT_SETTINGS_FOLDER } from '../constants/index'
 import { printMessage } from '../utils/message'
+import { FontFileType } from '../types/fontFile'
+
+const DEFAULT_PATH = `${DEFAULT_FOLDER}/${DEFAULT_FILE}`
 
 export default class File {
-  public static openFile: FileType = load(
+  public static getFile: FileType = load(
     readFileSync(`${DEFAULT_FOLDER}/${DEFAULT_FILE}`, {
       encoding: 'utf8',
       flag: 'r'
     })) as FileType
 
-  public static writeFile = (file: FileType) => {
-    writeFile(`${DEFAULT_FOLDER}/${DEFAULT_FILE}`, dump(file), err => {
+  public static writeFile = (file: any, path: string = DEFAULT_PATH) => {
+    writeFile(path, dump(file), err => {
       if (err) {
         const msg = 'an error occurred while trying to save changes'
         throw Error(printMessage('red', msg) || msg)
       }
     })
+  }
+
+  static openFile = (path: string) => {
+    return load(readFileSync(path, {
+      encoding: 'utf8',
+      flag: 'r'
+    }))
   }
 
   static setOpacity = (opacity: number, file: FileType) => {
@@ -57,5 +67,16 @@ export default class File {
         style: 'Bold Italic'
       }
     }
+  }
+
+  static setNewFont = (name: Array<string>) => {
+    const newFont = Object.fromEntries([name])
+
+    const path = `${DEFAULT_SETTINGS_FOLDER}/fonts.yml`
+    const customFontFile = File.openFile(path) as FontFileType
+
+    customFontFile.fonts = { ...customFontFile.fonts, ...newFont }
+
+    File.writeFile(customFontFile, path)
   }
 }
